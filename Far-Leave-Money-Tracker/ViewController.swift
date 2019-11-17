8a97a60b8e771adb712aa906c7692aee26b09a19
sinhchhinh <sinhchhinh@gmail.com>
2019-11-17 10:18:25 -0500

    Initial Commit

---
+++ Far-Leave-Money-Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Far-Leave-Money-Tracker
+//
+//  Created by Sinhchhinh Lor on 11/17/19.
+//  Copyright © 2019 PanPenFamily. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
